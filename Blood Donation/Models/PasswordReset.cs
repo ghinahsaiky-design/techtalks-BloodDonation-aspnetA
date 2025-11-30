362f90a6efcffe0543ec5e521a689942ed1f4286
Yamen Nasr <yamennasr2011@gmail.com>
2025-11-30 19:58:24 +0200

    Add PasswordReset model

---
+++ Blood Donation/Models/PasswordReset.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BloodDonation.Models
+{
+    public class PasswordReset
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public Users User { get; set; }
+
+        // 6-digit code
+        [Required]
+        [MaxLength(6)]
+        public string Code { get; set; }   // e.g. "482913"
+
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        [Required]
+        public DateTime ExpiresAt { get; set; }
+
+        [Required]
+        public bool IsUsed { get; set; } = false;
+      
+    }
+}
