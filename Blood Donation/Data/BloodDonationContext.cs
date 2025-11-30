51802d2f1c1991cf4f08a0d12973fcb292b5cf13
Yamen Nasr <yamennasr2011@gmail.com>
2025-11-30 18:45:50 +0200

    Add DbContext and connection string before migration

---
+++ Blood Donation/Data/BloodDonationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BloodDonation.Models;
+using System.Collections.Generic;
+
+namespace BloodDonation.Data
+{
+    public class BloodDonationContext : DbContext
+    {
+        public BloodDonationContext(DbContextOptions<BloodDonationContext> options) : base(options) { }
+
+        public DbSet<Users> Users { get; set; }
+        public DbSet<DonorProfile> DonorProfiles { get; set; }
+        public DbSet<BloodType> BloodTypes { get; set; }
+        public DbSet<Location> Locations { get; set; }
+    }
+}
