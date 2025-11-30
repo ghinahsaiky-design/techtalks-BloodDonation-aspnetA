256fe5f4d8fa32751a2b53e9ce919cab568a1c31
Yamen Nasr <yamennasr2011@gmail.com>
2025-11-30 18:25:42 +0200

    Add BloodType model

---
+++ Blood Donation/Models/BloodType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BloodDonation.Models
+{
+    public class BloodType
+    {
+        [Key]
+        public int BloodTypeId { get; set; }
+
+        [Required]
+        [MaxLength(3)]
+        public string Type { get; set; } 
+    }
+}
