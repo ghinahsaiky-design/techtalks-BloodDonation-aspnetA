beac88c12ddd61abd1c42b07185ac0937613f4b4
Yamen Nasr <yamennasr2011@gmail.com>
2025-11-30 18:26:05 +0200

    Add Location model

---
+++ Blood Donation/Models/Location.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BloodDonation.Models
+{
+    public class Location
+    {
+        [Key]
+        public int LocationId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string City { get; set; } // e.g., city, region, or area
+    }
+}
