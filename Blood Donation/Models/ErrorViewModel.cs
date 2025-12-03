42db2f811dcee65cfbbe1ce7f1fac80a7956b75c
Yamen-Nase <yamennasr2011@gmail.com>
2025-12-03 21:05:40 +0200

    Final Fixed Database

--- Blood Donation/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BloodDonation.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
