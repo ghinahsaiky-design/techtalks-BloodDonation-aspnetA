d78ee01363f206dde92e2a3dce5404251ed90f61
Yamen-Nase <yamennasr2011@gmail.com>
2025-12-04 21:38:14 +0200

    New Migration Applied

---
+++ Migrations/20251204193719_Initial.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BloodDonation.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BloodTypes",
+                columns: table => new
+                {
+                    BloodTypeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BloodTypes", x => x.BloodTypeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    LocationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Districts = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.LocationId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    Role = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DonorProfile",
+                columns: table => new
+                {
+                    DonorId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    BloodTypeId = table.Column<int>(type: "int", nullable: false),
+                    LocationId = table.Column<int>(type: "int", nullable: false),
+                    IsHealthyForDonation = table.Column<bool>(type: "bit", nullable: false),
+                    IsIdentityHidden = table.Column<bool>(type: "bit", nullable: false),
+                    IsAvailable = table.Column<bool>(type: "bit", nullable: false),
+                    LastDonationDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DonorProfile", x => x.DonorId);
+                    table.ForeignKey(
+                        name: "FK_DonorProfile_BloodTypes_BloodTypeId",
+                        column: x => x.BloodTypeId,
+                        principalTable: "BloodTypes",
+                        principalColumn: "BloodTypeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DonorProfile_Locations_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "Locations",
+                        principalColumn: "LocationId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DonorProfile_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PasswordResets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(6)", maxLength: 6, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ExpiresAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsUsed = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PasswordResets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PasswordResets_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "BloodTypes",
+                columns: new[] { "BloodTypeId", "Type" },
+                values: new object[,]
+                {
+                    { 1, "A+" },
+                    { 2, "A-" },
+                    { 3, "B+" },
+                    { 4, "B-" },
+                    { 5, "AB+" },
+                    { 6, "AB-" },
+                    { 7, "O+" },
+                    { 8, "O-" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Locations",
+                columns: new[] { "LocationId", "Districts" },
+                values: new object[,]
+                {
+                    { 1, "Beirut" },
+                    { 2, "Baabda" },
+                    { 3, "Aley" },
+                    { 4, "Chouf" },
+                    { 5, "Keserwan" },
+                    { 6, "Matn" },
+                    { 7, "Jbeil" },
+                    { 8, "Zahle" },
+                    { 9, "Baalbek" },
+                    { 10, "Hermel" },
+                    { 11, "Tripoli" },
+                    { 12, "Miniyeh-Danniyeh" },
+                    { 13, "Zgharta" },
+                    { 14, "Koura" },
+                    { 15, "Batroun" },
+                    { 16, "Bcharre" },
+                    { 17, "Akkar" },
+                    { 18, "Saida" },
+                    { 19, "Tyre" },
+                    { 20, "Jezzine" },
+                    { 21, "Nabatieh" },
+                    { 22, "Bint Jbeil" },
+                    { 23, "Marjeyoun" },
+                    { 24, "Hasbaya" },
+                    { 25, "Rachaya" },
+                    { 26, "West Beqaa" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DonorProfile_BloodTypeId",
+                table: "DonorProfile",
+                column: "BloodTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DonorProfile_LocationId",
+                table: "DonorProfile",
+                column: "LocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DonorProfile_UserId",
+                table: "DonorProfile",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PasswordResets_UserId",
+                table: "PasswordResets",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DonorProfile");
+
+            migrationBuilder.DropTable(
+                name: "PasswordResets");
+
+            migrationBuilder.DropTable(
+                name: "BloodTypes");
+
+            migrationBuilder.DropTable(
+                name: "Locations");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
