0edb88a3283aa76d6032ac1e8de79235818df712
Ghina <ghinahsaiky@gmail.com>
2025-12-06 22:24:38 +0200

    Fixed the redirection of the buttons and added the dashboardController

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BloodDonation.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult SearchDonors()
+        {
+            return View();
+        }
+    }
+}
