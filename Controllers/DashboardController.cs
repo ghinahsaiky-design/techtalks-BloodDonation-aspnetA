8aa720e8e385b773a8120ce6421b4881b454fca3
Rifaat Ramadan <rifaatramadan0@gmail.com>
2025-12-08 16:51:28 +0200

    Added SearchDonors to DashboardController

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -1,12 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BloodDonation.Data;
+using BloodDonation.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 
 namespace BloodDonation.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly BloodDonationContext _context;
+
+        public DashboardController(BloodDonationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
         public IActionResult SearchDonors()
         {
-            return View();
+            var model = new SearchViewModel();
+
+            // Fill dropdowns
+            model.Locations = _context.Locations
+                .OrderBy(l => l.Districts)
+                .Select(l => new SelectListItem
+                {
+                    Value = l.LocationId.ToString(),
+                    Text = l.Districts
+                });
+
+            model.BloodTypes = _context.BloodTypes
+                .OrderBy(b => b.Type)
+                .Select(b => new SelectListItem
+                {
+                    Value = b.BloodTypeId.ToString(),
+                    Text = b.Type
+                });
+
+            return View(model);
         }
     }
 }
