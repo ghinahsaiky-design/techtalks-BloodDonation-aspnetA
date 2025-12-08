918ad5b0ee34692fef43154b14f835b24fc598e4
Rifaat Ramadan <rifaatramadan0@gmail.com>
2025-12-08 16:41:43 +0200

    Added 2 files (SearchResultViewModel + SearchViewModel)

---
+++ Models/SearchResultViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BloodDonation.Models
+{
+    public class SearchResultViewModel
+    {
+        public string DonorName { get; set; }
+        public string BloodType { get; set; }
+        public string City { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
