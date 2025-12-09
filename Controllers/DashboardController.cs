2ae23a08439f619f043791a3ac16f5b108b47a12
Rifaat Ramadan <rifaatramadan0@gmail.com>
2025-12-09 12:13:40 +0200

    Added POST SearchDonors action to DashboardController

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -39,5 +39,51 @@ namespace BloodDonation.Controllers
 
             return View(model);
         }
+
+        
+        [HttpPost]
+        public IActionResult SearchDonors(SearchViewModel model)
+        {
+            // Refill dropdowns
+            model.Locations = _context.Locations
+                .OrderBy(l => l.Districts)
+                .Select(l => new SelectListItem
+                {
+                    Value = l.LocationId.ToString(),
+                    Text = l.Districts
+                });
+
+            model.BloodTypes = _context.BloodTypes
+                .OrderBy(b => b.Type)
+                .Select(b => new SelectListItem
+                {
+                    Value = b.BloodTypeId.ToString(),
+                    Text = b.Type
+                });
+
+            // Build query
+            var query = _context.DonorProfile.AsQueryable();
+
+            if (model.SelectedLocationId.HasValue)
+                query = query.Where(d => d.LocationId == model.SelectedLocationId.Value);
+
+            if (model.SelectedBloodTypeId.HasValue)
+                query = query.Where(d => d.BloodTypeId == model.SelectedBloodTypeId.Value);
+
+
+            model.Results = query
+                .Select(d => new SearchResultViewModel
+                {
+                    DonorName = d.User.FirstName + " " + d.User.LastName,
+                    BloodType = d.BloodType.Type,
+                    City = d.Location.Districts,
+                    PhoneNumber = d.User.Phone,
+                    Email = d.User.Email,
+                    IsAvailable = d.IsAvailable
+                }).ToList();
+
+            return View(model);
+        }
+
     }
 }
